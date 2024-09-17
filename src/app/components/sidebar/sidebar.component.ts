ec6d61f1be51c6b1cb41299e6029f97e824172e4
kewablay <kojokewajunior123@gmail.com>
2024-09-17 15:05:38 +0000

    added sidebar with board list and active board states

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Board } from '../../models/app.model';
+import { selectBoards } from '../../state/boards/selectors/boards.selectors';
+import { AsyncPipe } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [ AsyncPipe, RouterLink, RouterLinkActive],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.sass'
+  styleUrl: './sidebar.component.sass',
 })
 export class SidebarComponent {
+  boards$: Observable<Board[]>;
+  constructor(private store: Store) {
+    this.boards$ = this.store.select(selectBoards);
+  }
 
 }
--- src/app/pages/board-detail/board-detail.component.ts
+++ src/app/pages/board-detail/board-detail.component.ts
@@ -1,19 +1,13 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { selectBoard } from '../../state/boards/selectors/boards.selectors';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-board-detail',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './board-detail.component.html',
   styleUrl: './board-detail.component.sass',
 })
-export class BoardDetailComponent {
-  boards$ = this.store.select(selectBoard);
-  constructor(private store: Store) {}
-
-  ngOnInit() {
-  this.boards$.subscribe((boards) => console.log('boards from store', boards))
-  }
-}
+export class BoardDetailComponent {}
