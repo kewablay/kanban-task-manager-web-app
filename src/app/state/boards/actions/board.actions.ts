fc5a51c4dc0f29caefe289e61e8db7cda06bd067
kewablay <kojokewajunior123@gmail.com>
2024-09-17 11:41:55 +0000

    added board actions

---
+++ src/app/state/boards/actions/board.actions.ts
@@ -0,0 +1,33 @@
+import { createAction, props } from '@ngrx/store';
+import { Board } from '../../../models/app.model';
+
+// CREATE
+export const addBoard = createAction(
+  '[Board] Add Board',
+  props<{ board: Board }>()
+);
+
+// READ
+export const loadBoards = createAction('[Board] Load Boards');
+export const loadBoardsSuccess = createAction(
+  '[Board] Load Boards Success',
+  props<{ boards: Board[] }>()
+);
+export const loadBoardsError = createAction(
+  '[Board] Load Boards Error',
+  props<{ error: string }>()
+);
+
+// UPDATE
+export const updateBoard = createAction(
+  '[Board] Update Board',
+  props<{ board: Partial<Board> & { id: string } }>()
+);
+
+// DELETE
+export const deleteBoard = createAction(
+  '[Board] Delete Board',
+  props<{ boardId: string }>()
+);
+
+export const test = createAction('[Board] Test');
