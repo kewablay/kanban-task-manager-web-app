de5e81fe89b5bf1b767493011eaea0f7f9b1d9ca
kewablay <kojokewajunior123@gmail.com>
2024-09-17 09:10:31 +0000

    testing data service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataServiceService } from './services/data-service/data-service.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.sass',
 })
 export class AppComponent {
-  title = 'kanban-task-management-web-app';
+  constructor(private dataService: DataServiceService) {
+    this.dataService.fetchData().subscribe((data) => console.log('data:', data));
+  }
 }
