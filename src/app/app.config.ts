210b0d90fc1a88e73b74fd2d2e129df42899ecb0
kewablay <kojokewajunior123@gmail.com>
2024-09-17 14:43:38 +0000

    added app routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,7 @@ import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideState, provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
-import { provideRouterStore } from '@ngrx/router-store';
+import { provideRouterStore, routerReducer } from '@ngrx/router-store';
 import { BoardEffects } from './state/boards/effects/board.effects';
 import { boardReducer } from './state/boards/reducer/board.reducers';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
@@ -16,7 +16,9 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    provideStore(),
+    provideStore({
+      router: routerReducer,
+    }),
     provideState({ name: 'boards', reducer: boardReducer }),
     provideState({ name: 'theme', reducer: themeReducer }),
     provideEffects(BoardEffects, themeEffect),
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'board/:id',
+    loadComponent: () =>
+      import('./pages/board-detail/board-detail.component').then(
+        (m) => m.BoardDetailComponent
+      ),
+  },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/board-redirect/board-redirect.component').then(
+        (m) => m.BoardRedirectComponent
+      ),
+  },
+];
