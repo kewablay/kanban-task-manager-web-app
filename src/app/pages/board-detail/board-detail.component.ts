05a23a8122376f6d26bf4197cf669c72570c099c
kewablay <kojokewajunior123@gmail.com>
2024-09-17 15:35:09 +0000

    updated board detail to list the boards contents

--- src/app/pages/board-detail/board-detail.component.ts
+++ src/app/pages/board-detail/board-detail.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { selectBoard } from '../../state/boards/selectors/boards.selectors';
+import { selectBoardWithParamId } from '../../state/boards/selectors/boards.selectors';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { BoardContentComponent } from '../../components/board-content/board-content.component';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-board-detail',
   standalone: true,
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, BoardContentComponent, AsyncPipe],
   templateUrl: './board-detail.component.html',
   styleUrl: './board-detail.component.sass',
 })
-export class BoardDetailComponent {}
+export class BoardDetailComponent {
+  board$ = this.store.select(selectBoardWithParamId);
+  constructor(private store: Store) {}
+
+  ngOnInit() {
+    this.board$.subscribe((board) => {
+      console.log('border with params', board);
+    });
+  }
+}
