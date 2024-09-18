f997d5204a6a4d19ff9e2a9e29ee71a371ee61e1
kewablay <kojokewajunior123@gmail.com>
2024-09-18 12:08:58 +0000

    updated header component to open task form

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,26 +5,32 @@ import { BoardFormComponent } from '../../shared/board-form/board-form.component
 import { Store } from '@ngrx/store';
 import { deleteBoard } from '../../state/boards/actions/board.actions';
 import { Router } from '@angular/router';
+import { TaskFormComponent } from '../../shared/task-form/task-form.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [AsyncPipe, BoardFormComponent],
+  imports: [AsyncPipe, BoardFormComponent, TaskFormComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.sass',
 })
 export class HeaderComponent {
   @Input() board!: Board;
-  isEditBoardModalOpen = false;
+  isEditBoardModalOpen: boolean = false;
+  isTaskModalOpen: boolean = false;
 
-  constructor(private store: Store,private router: Router) {}
+  constructor(private store: Store, private router: Router) {}
 
   openEditBoardModal() {
     this.isEditBoardModalOpen = !this.isEditBoardModalOpen;
+  }
+  openTaskModal() {
+    this.isTaskModalOpen = !this.isTaskModalOpen;
   }
 
   deleteBoard(id: number) {
     this.store.dispatch(deleteBoard({ boardId: id }));
     this.router.navigate(['/']);
   }
+
 }
