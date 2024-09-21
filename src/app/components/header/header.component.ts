e3413f41a3bf008f7fa5942d5537e86694507f8a
kewablay <kojokewajunior123@gmail.com>
2024-09-21 23:29:25 +0000

    added initial add task dialogue, board menu, and edit board dialog

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,11 +6,20 @@ import { Store } from '@ngrx/store';
 import { deleteBoard } from '../../state/boards/actions/board.actions';
 import { Router } from '@angular/router';
 import { TaskFormComponent } from '../../shared/task-form/task-form.component';
-
+import { CdkMenu, CdkMenuItem, CdkMenuTrigger } from '@angular/cdk/menu';
+import {Dialog, DialogModule} from '@angular/cdk/dialog';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [AsyncPipe, BoardFormComponent, TaskFormComponent],
+  imports: [
+    AsyncPipe,
+    BoardFormComponent,
+    TaskFormComponent,
+    CdkMenuTrigger,
+    CdkMenu,
+    CdkMenuItem,
+    DialogModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.sass',
 })
@@ -19,18 +28,23 @@ export class HeaderComponent {
   isEditBoardModalOpen: boolean = false;
   isTaskModalOpen: boolean = false;
 
-  constructor(private store: Store, private router: Router) {}
+  constructor(private store: Store, private router: Router,   public dialog: Dialog) {}
 
   openEditBoardModal() {
-    this.isEditBoardModalOpen = !this.isEditBoardModalOpen;
+    this.dialog.open(BoardFormComponent, {
+      width: '480px',
+      data: this.board
+    });
   }
-  openTaskModal() {
-    this.isTaskModalOpen = !this.isTaskModalOpen;
+  
+  openCreateTaskModal() {
+    this.dialog.open(TaskFormComponent, {
+      width: '480px',
+    });
   }
 
   deleteBoard(id: number) {
     this.store.dispatch(deleteBoard({ boardId: id }));
     this.router.navigate(['/']);
   }
-
 }
