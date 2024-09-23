107009c0e6d487e49910e36054b8442697a3d18f
kewablay <kojokewajunior123@gmail.com>
2024-09-23 21:26:08 +0000

    added delete logic for task in delete dialog

--- src/app/shared/delete-dialog/delete-dialog.component.ts
+++ src/app/shared/delete-dialog/delete-dialog.component.ts
@@ -1,5 +1,14 @@
 import { DIALOG_DATA, DialogRef } from '@angular/cdk/dialog';
 import { Component, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+import {
+  deleteBoard,
+  deleteTask,
+} from '../../state/boards/actions/board.actions';
+import { Router } from '@angular/router';
+import { Board } from '../../models/app.model';
+import { Observable } from 'rxjs';
+import { selectBoardWithParamId } from '../../state/boards/selectors/boards.selectors';
 
 @Component({
   selector: 'app-delete-dialog',
@@ -9,16 +18,41 @@ import { Component, inject } from '@angular/core';
   styleUrl: './delete-dialog.component.sass',
 })
 export class DeleteDialogComponent {
+  board$: Observable<Board | null | undefined>;
+  boardId: number = 0;
   data = inject(DIALOG_DATA);
   private deletDialogRef = inject(DialogRef<DeleteDialogComponent>);
 
-  constructor() {}
+  constructor(private store: Store, private router: Router) {
+    this.board$ = this.store.select(selectBoardWithParamId);
+  }
 
-  onDeleteConfirm() {
-    this.deletDialogRef.close();  // Emit confirmation
+  ngOnInit() {
+    this.board$.subscribe((board) => {
+      if (board) {
+        this.boardId = board.id;
+      }
+    });
+  }
+
+  onDeleteConfirm(id: number) {
+    this.deletDialogRef.close(); // Emit confirmation
+    // delete data (which can be a board or a task )
+    if (this.data.columns) {
+      this.store.dispatch(deleteBoard({ boardId: id }));
+      this.router.navigate(['/']);
+    } else {
+      this.store.dispatch(
+        deleteTask({
+          boardId: this.boardId,
+          taskId: id,
+          columnName: this.data.status,
+        })
+      );
+    }
   }
 
   onCancel(): void {
-    this.deletDialogRef.close();  // Emit cancellation
+    this.deletDialogRef.close(); // Emit cancellation
   }
 }
