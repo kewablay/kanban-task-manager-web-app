8aab97d83467614a27bdc1d2d5d6c69e5fce14b4
kewablay <kojokewajunior123@gmail.com>
2024-09-24 10:31:48 +0000

    added sidebar component for mobile

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,8 +7,9 @@ import { deleteBoard } from '../../state/boards/actions/board.actions';
 import { Router } from '@angular/router';
 import { TaskFormComponent } from '../../shared/task-form/task-form.component';
 import { CdkMenu, CdkMenuItem, CdkMenuTrigger } from '@angular/cdk/menu';
-import {Dialog, DialogModule} from '@angular/cdk/dialog';
+import { Dialog, DialogModule } from '@angular/cdk/dialog';
 import { DeleteDialogComponent } from '../../shared/delete-dialog/delete-dialog.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 @Component({
   selector: 'app-header',
   standalone: true,
@@ -19,7 +20,7 @@ import { DeleteDialogComponent } from '../../shared/delete-dialog/delete-dialog.
     CdkMenuTrigger,
     CdkMenu,
     CdkMenuItem,
-    DialogModule
+    DialogModule,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.sass',
@@ -29,30 +30,40 @@ export class HeaderComponent {
   isEditBoardModalOpen: boolean = false;
   isTaskModalOpen: boolean = false;
 
-  constructor(private store: Store, private router: Router,   public dialog: Dialog) {}
+  constructor(
+    private store: Store,
+    private router: Router,
+    public dialog: Dialog
+  ) {}
 
   openEditBoardModal() {
     this.dialog.open(BoardFormComponent, {
       width: '85%',
       maxWidth: '480px',
-      data: this.board
+      data: this.board,
     });
   }
-  
+
   openCreateTaskModal() {
     this.dialog.open(TaskFormComponent, {
       width: '85%',
-      maxWidth: '480px'
+      maxWidth: '480px',
+    });
+  }
+
+  openSideBarMenu() {
+    this.dialog.open(SidebarComponent, {
+      width: '85%',
+      maxWidth: '480px',
     });
   }
 
   deleteBoard(id: number) {
-
     this.dialog.open(DeleteDialogComponent, {
       width: '85%',
-      maxWidth: '480px', 
-      data: this.board
-    })
+      maxWidth: '480px',
+      data: this.board,
+    });
     // this.store.dispatch(deleteBoard({ boardId: id }));
     // this.router.navigate(['/']);
   }
