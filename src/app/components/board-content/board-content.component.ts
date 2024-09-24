3eb161bc1547a8fdb02415c7d4639a4fa46eefe9
kewablay <kojokewajunior123@gmail.com>
2024-09-24 11:14:19 +0000

    added add column button

--- src/app/components/board-content/board-content.component.ts
+++ src/app/components/board-content/board-content.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { Board } from '../../models/app.model';
 import { TaskComponent } from "../task/task.component";
+import { Dialog } from '@angular/cdk/dialog';
+import { BoardFormComponent } from '../../shared/board-form/board-form.component';
 
 @Component({
   selector: 'app-board-content',
@@ -11,4 +13,15 @@ import { TaskComponent } from "../task/task.component";
 })
 export class BoardContentComponent {
   @Input() board!: Board;
+
+
+  constructor( public dialog: Dialog) {}
+
+  openEditBoardModal() {
+    this.dialog.open(BoardFormComponent, {
+      width: '85%',
+      maxWidth: '480px',
+      data: this.board,
+    });
+  }
 }
